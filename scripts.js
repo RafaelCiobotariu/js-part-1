acc95023c6496ec880fd6ec8f6c2df62173a682b
Rafael Ciobotariu <gheorghe.ciobotariu99@e-uvt.ro>
2022-12-07 16:23:39 +0200

    Updated script.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,38 @@
+alert(
+  'Hello.  This is a "Rock, paper, scissors" game. You will have to select the one of the choices above and after that, the computer will give one random answer, from the choices above. '
+);
+
+let gameRunning;
+
 function playerPlay() {
-  var playerChoice = prompt('insert a value from "rock" "paper" "scissors"');
-  playerChoice = playerChoice.toLowerCase().trim();
-  if (
-    playerChoice !== "rock" &&
-    playerChoice !== "paper" &&
-    playerChoice !== "scissors"
-  ) {
-    return console.log(
-      "You inserted a wrong word, please refresh the page to play again!"
-    );
+  let playerChoice = prompt('insert a value from "rock" "paper" "scissors"');
+  let exitGame;
+  if (playerChoice == null) {
+    exitGame = prompt("You really want to exit the game? \n Write 'yes'");
   }
-  console.log(playerChoice);
-  return playerChoice;
+  if (exitGame === "yes") {
+    gameRunning = 0;
+  }
+  // console.log(gameRunning);
+  if (gameRunning !== 0) {
+    playerChoice = playerChoice.toLowerCase().trim();
+    if (
+      playerChoice !== "rock" &&
+      playerChoice !== "paper" &&
+      playerChoice !== "scissors"
+    ) {
+      console.log(
+        "You inserted a wrong word, please refresh the page to play again!"
+      );
+      playerPlay();
+    }
+
+    console.log(playerChoice);
+    return playerChoice;
+  } else {
+    // console.log(gameRunning);
+    alert("Game Finished");
+  }
 }
 
 function computerPlay() {
@@ -73,18 +94,29 @@ function playRound(playerSelection, computerSelection) {
 function game(numberOfRunds) {
   let playerScore = 0,
     computerScore = 0;
-  for (let i = 0; i < numberOfRunds; i++) {
-    let gameResult = playRound(playerPlay(), computerPlay());
-    if (gameResult === "Player won!") {
-      playerScore++;
-    } else if (gameResult === "Computer won!") {
-      computerScore++;
-    } else {
-      console.log("No points added because it was a draw");
+  if (gameRunning !== 0) {
+    for (let i = 0; i < numberOfRunds; i++) {
+      console.log(`Round ${i}:`);
+      let gameResult = playRound(playerPlay(), computerPlay());
+      if (gameResult === "Player won!") {
+        playerScore++;
+      } else if (gameResult === "Computer won!") {
+        computerScore++;
+      } else if (gameResult === "Draw!") {
+        console.log("No points added because it was a draw");
+      } else {
+        console.log("No points added because some problems");
+      }
+      console.log("\n");
     }
-    console.log("\n");
+    console.log(
+      "Player score: " +
+        playerScore +
+        " ---- " +
+        computerScore +
+        " Computer score"
+    );
   }
-  console.log(playerScore + " " + computerScore);
 }
 
-game(4);
+game(5);
